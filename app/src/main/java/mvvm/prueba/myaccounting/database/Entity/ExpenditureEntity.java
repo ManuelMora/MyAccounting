0e020844d7cbf36979dee62fd5cabb442bc05de5
Manuel <al_mora23@hotmail.com>
2020-02-13 09:14:26 -0500

    Added Expenditure Foreign Keys and indices

--- app/src/main/java/mvvm/prueba/myaccounting/database/Entity/ExpenditureEntity.java
+++ app/src/main/java/mvvm/prueba/myaccounting/database/Entity/ExpenditureEntity.java
@@ -1,15 +1,33 @@
 package mvvm.prueba.myaccounting.database.Entity;
 
+import androidx.room.ColumnInfo;
 import androidx.room.Entity;
+import androidx.room.ForeignKey;
+import androidx.room.Index;
 import androidx.room.PrimaryKey;
 
 import mvvm.prueba.myaccounting.model.Expenditure;
 
-@Entity(tableName = "expenditure")
+@Entity(tableName = "expenditure",
+foreignKeys = {
+    @ForeignKey(entity = CycleEntity.class,
+    parentColumns = "id",
+    childColumns = "id_cycle",
+    onDelete = ForeignKey.RESTRICT),
+    @ForeignKey(entity = ActualEntity.class,
+    parentColumns = "id",
+    childColumns = "id",
+    onDelete = ForeignKey.RESTRICT)
+}, indices = {
+    @Index(value = "id_actual"),
+    @Index(value = "id_actual")
+})
 public class ExpenditureEntity implements Expenditure {
   @PrimaryKey(autoGenerate = true)
   private int id;
+  @ColumnInfo(name = "id_actual")
   private int actualId;
+  @ColumnInfo(name = "id_cycle")
   private int cycleId;
   private String name;
   private double value;
